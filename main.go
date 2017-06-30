c6183a69dc93ae5ddd907bd45991fe4b24f3e84e
dmitsov <donimicov@gmail.com>
2017-06-30 15:07:53 +0300

    * test push

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ import (
 
 var (
 	maxIter                            = 700
-	rMin                               = -1.
-	rMax                               = 1.
-	iMin                               = -1.
-	iMax                               = 0.
+	rMin                               = -2.
+	rMax                               = 2.
+	iMin                               = -2.
+	iMax                               = 2.
 	imgWidth                           = 640
-	imgHeight                          = 480
+	imgHeight                          = 640
 	grtnsCount                         = 1
 	outputFile                         = "zad18.png"
 	funcMap    map[string]func(string) = map[string]func(string){
@@ -35,12 +35,14 @@ var (
 		"-s":      setSize,
 		"-size":   setSize,
 	}
+	
+	isQuiet bool = false
 )
 
 func mandelbrot(a complex128) color.Color {
 	i := 0
-	for z := a; cmplx.Abs(z) < 90 && i < maxIter; i++ {
-		z = cmplx.Exp(cmplx.Cos(a*z))
+	for z := a; cmplx.Abs(z) < 50 && i < maxIter; i++ {
+		z = cmplx.Exp(cmplx.Cos(z * a))
 	}
 	
 	k := i / maxIter;
@@ -124,19 +126,24 @@ func main() {
 
 	args := os.Args[1:]
 	
-	runtime.GOMAXPROCS(32)
+	fmt.Println("Max threads: ",runtime.GOMAXPROCS(32))
 	
 	for i := 0; i < len(args); i += 2 {
-		funcMap[args[i]](args[i+1])
+		if args[i] == "-q" || args[i] == "-quiet" {
+			isQuiet = true
+		} else {
+			funcMap[args[i]](args[i+1])
+		}
 	}
 
 	bounds := image.Rect(0, 0, imgWidth, imgHeight)
 	img := image.NewRGBA(bounds)
 	currentTime := time.Now()
 	
-	fmt.Printf("Threads used in current run: %d\n", grtnsCount)
-
-	granularity := grtnsCount*4
+	if !isQuiet {
+		fmt.Printf("Threads used in current run: %d\n", grtnsCount)
+	}
+	granularity := grtnsCount
 	
 	if grtnsCount == 1 {
 		mandelbrotSet(img, 0, 0, imgWidth, imgHeight)
@@ -166,14 +173,16 @@ func main() {
 		}
 
 		if rowNum > granularity {
-			for i := 0; i < columnNum; i++ {
-				blockChan[granularity*granularity+i] = []int{i * blockWidth, granularity * blockHeight, blockWidth, imgHeight - blockHeight*granularity}
+			for i := 0; i < granularity; i++ {
+				blockChan[granularity*granularity+i] = []int{i * blockWidth, granularity * blockHeight, blockWidth, imgHeight%granularity}
 			}
+			
+			blockChan[granularity*granularity + granularity] = []int{granularity * blockWidth, granularity * blockHeight, imgWidth % granularity, imgHeight % granularity}
 		}
 
 		if columnNum > granularity {
 			for i := 0; i < granularity; i++ {
-				blockChan[granularity*granularity+columnNum+i] = []int{granularity * blockWidth, i * blockHeight, imgWidth - granularity*blockWidth, blockHeight}
+				blockChan[granularity*granularity+columnNum+i] = []int{granularity * blockWidth, i * blockHeight, imgWidth % granularity, blockHeight}
 			}
 		}
 
@@ -181,26 +190,37 @@ func main() {
 
 		// fmt.Println("Block count ", rowNum*columnNum)
 
-		for i := 0; i < grtnsCount; i++ {
+		for i := 0; i < grtnsCount - 1; i++ {
 			wg.Add(1)
 			go func(id int, blocks [][]int) {
 				defer wg.Done()
-				runtime.Gosched()
+				//runtime.Gosched()
 				grtnTime := time.Now()
-				fmt.Printf("Thread %d started\n", id)
+				if !isQuiet {
+					fmt.Printf("Thread %d started\n", id)
+				}
+				
 				for blockCount < columnNum*rowNum {
 					block := blocks[blockCount]
 					blockCount++
 					mandelbrotSet(img, block[0], block[1], block[2], block[3])
 				}
-				fmt.Printf("Thread %d stopped\n", id)
-				fmt.Printf("Thread %d execution time %fs\n", id, time.Since(grtnTime).Seconds()	 * 1000)
+				
+				if !isQuiet {
+					fmt.Printf("Thread %d stopped\n", id)
+					fmt.Printf("Thread %d execution time %fms\n", id, time.Since(grtnTime).Seconds()* 1000)
+				}
 				
 			}(i, blockChan)
 		}
 
 		// fmt.Println("Goroutines started")
-
+		for blockCount < columnNum*rowNum {
+			block := blockChan[blockCount]
+			blockCount++
+			mandelbrotSet(img, block[0], block[1], block[2], block[3])
+		}
+		
 		wg.Wait()
 
 	}
